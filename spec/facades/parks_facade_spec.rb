437c2155923690e835fdfe584f7966966f06fdd8
Eli Sachs <easachs13@gmail.com>
2022-09-14 22:10:11 -0600

    Test: improve facade specs

--- spec/facades/parks_facade_spec.rb
+++ spec/facades/parks_facade_spec.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'ParksFacade' do
   it 'returns instances of parks', vcr: 'austin_parks' do
-    parks = ParksFacade.parks_near("Austin")
+    parks = ParksFacade.parks_near('Austin')
     expect(parks).to be_a(Array)
     expect(parks).to be_all(Park)
+    expect(parks.length).to eq(3)
   end
 end
--- spec/facades/restaurants_facade_spec.rb
+++ spec/facades/restaurants_facade_spec.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe 'RestaurantsFacade' do
-    it 'returns instances of restaurants' do
-        restaurants = RestaurantsFacade.restaurants_near("Austin")
-        expect(restaurants).to be_a(Array)
-        expect(restaurants).to be_all(Restaurants)
-    end
+  it 'returns instances of restaurants', vcr: 'austin_restaurants' do
+    restaurants = RestaurantsFacade.restaurants_near('Austin')
+    expect(restaurants).to be_a(Array)
+    expect(restaurants).to be_all(Restaurant)
+    expect(restaurants.length).to eq(3)
+  end
 end
