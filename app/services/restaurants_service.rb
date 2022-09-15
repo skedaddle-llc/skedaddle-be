984875dd50364abd990c36f7b45884dae98c22c0
Nick Jones <nickjones9605@gmail.com>
2022-09-15 09:30:02 -0600

    Adds env token to conn method

--- app/services/restaurants_service.rb
+++ app/services/restaurants_service.rb
@@ -6,7 +6,7 @@ class RestaurantsService
 
     def self.conn
         Faraday.new(url: 'https://api.yelp.com/v3/businesses') do |faraday|
-        faraday.headers['authorization'] = 'Bearer CiVfkw3Ed9hpNA0K27ZJ2JxFF2fcjU_YxVypgceHG6mDP2Zj4DzeHnKaXv5MMLm7BxkL8D1KvbReCXpeSeWgyBkd2ulVCoQ1Rt3pZx5JZ1surjBrIAfqpxLuZxEgY3Yx'
+            faraday.headers['authorization'] = "Bearer #{ENV['yelp_api_key']}"
         end
     end
 
