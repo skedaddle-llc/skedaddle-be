a66c89b5a3fcec4f2a03d2be9ae9b51e67b0a579
Nick Jones <nickjones9605@gmail.com>
2022-09-15 14:09:19 -0600

    Adds testing for a successful response header for restaurantsservice

--- spec/services/restaurants_service_spec.rb
+++ spec/services/restaurants_service_spec.rb
@@ -1,29 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe 'RestaurantsService', :vcr do
+
+    it "returns a 200 success header when the API call is made" do
+        response = RestaurantsService.restaurants_near("Denver")
+        expect(response.first[1]).to_not(eq(nil))
+    end
     it 'can return restaurants when the call is made' do
         parse_json = RestaurantsService.restaurants_near("Denver")
 
         expect(parse_json).to be_a(Hash)
-        # expect(parse_json.first[:title]).to eq("The Godfather")
-        # expect(parse_json.count).to eq(40)
         expect(parse_json.first[1][0]).to include(:name, :rating, :phone, :categories)
     end
 
-#   it 'can display search results' do
-#     parse_json = MovieService.search_movies('Phoenix')
+    it 'can SAD PATH' do
+        parse_json = RestaurantsService.restaurants_near(nil)
 
-#     expect(parse_json).to be_a(Array)
-#     expect(parse_json.first).to include(:id, :title, :vote_average)
-#     expect(parse_json.count).to eq(20)
-#   end
-
-#   it 'can get a movie' do
-#     parse_json = MovieService.get_movie(238)
-
-#     expect(parse_json).to be_a(Hash)
-#     expect(parse_json[:title]).to eq('The Godfather')
-#     expect(parse_json).to include(:id, :title, :runtime, :poster_path, :vote_average, :genres, :overview, :cast,
-#                                   :release_date, :results)
-#   end
+        expect(parse_json).to_not be_a(Array)
+    end
 end
