ae1c56950870568b6ed8152b684fbaaa085357c7
Eli Sachs <easachs13@gmail.com>
2022-09-16 13:56:45 -0600

    Test: update facade specs

--- spec/facades/restaurants_facade_spec.rb
+++ spec/facades/restaurants_facade_spec.rb
@@ -3,15 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe 'RestaurantsFacade' do
-    it 'returns instances of restaurants', vcr: 'austin_restaurants' do
-        restaurants = RestaurantsFacade.restaurants_near('Austin')
-        expect(restaurants).to be_a(Array)
-        expect(restaurants).to be_all(Restaurant)
-        expect(restaurants.length).to eq(3)
-    end
+  it 'returns instances of restaurants', vcr: 'denver_restaurants' do
+    restaurants = RestaurantsFacade.restaurants_near('Denver')
+    expect(restaurants).to be_a(Array)
+    expect(restaurants).to be_all(Restaurant)
+    expect(restaurants.length).to eq(3)
+  end
 
-    it 'errors gracefully', vcr: 'bad_restaurant_search' do
-        restaurants = RestaurantsFacade.restaurants_near('not a real place')
-        expect(restaurants).to eq([])
-    end
+  it 'errors gracefully', vcr: 'bad_restaurant_facade' do
+    restaurants = RestaurantsFacade.restaurants_near('')
+    expect(restaurants).to eq([])
+  end
 end
