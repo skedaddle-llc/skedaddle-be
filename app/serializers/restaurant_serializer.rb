dd8c308e41af3142b37ba02ccad150627ea8ae0b
Eli Sachs <easachs13@gmail.com>
2022-09-16 14:00:03 -0600

    Refactor: handroll serializers

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class RestaurantSerializer
-  attr_reader :resource
-  include JSONAPI::Serializer
-  attributes :name, :rating, :price, :image_url, :url
+  def self.format_restaurants(restaurants)
+    restaurants.map do |restaurant|
+      {
+        name: restaurant.name,
+        rating: restaurant.rating,
+        price: restaurant.price,
+        image_url: restaurant.image_url,
+        url: restaurant.url,
+        categories: restaurant.categories,
+        address: restaurant.address,
+        phone: restaurant.phone
+      }
+    end
+  end
 end
