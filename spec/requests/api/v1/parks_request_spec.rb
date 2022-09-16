5abb6a94f725b0bed572012a64309afbd8a14e14
Eli Sachs <easachs13@gmail.com>
2022-09-16 13:57:58 -0600

    Test: create request specs

---
+++ spec/requests/api/v1/parks_request_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Parks API' do
+  it 'sends a list of parks', vcr: 'denver_parks' do
+    get '/api/v1/parks?search=Denver'
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    parks = JSON.parse(response.body, symbolize_names: true)
+    expect(parks.count).to eq(3)
+    expect(parks).to be_a(Array)
+    expect(parks).to be_all(Hash)
+
+    parks.each do |park|
+      expect(park).to have_key(:name)
+      expect(park[:name]).to be_a(String)
+      expect(park).to have_key(:city)
+      expect(park[:city]).to be_a(String)
+      expect(park).to have_key(:state)
+      expect(park[:state]).to be_a(String)
+      expect(park).to have_key(:country)
+      expect(park[:country]).to be_a(String)
+      expect(park).to have_key(:description)
+      expect(park[:description]).to be_a(String)
+      expect(park).to have_key(:directions)
+      expect(park[:directions]).to be_a(String)
+      expect(park).to have_key(:lat)
+      expect(park[:lat]).to be_a(String)
+      expect(park).to have_key(:lon)
+      expect(park[:lon]).to be_a(String)
+      expect(park).to have_key(:activities)
+      expect(park[:activities]).to be_a(Array)
+    end
+  end
+end
--- spec/requests/api/v1/restaurants_request_spec.rb
+++ spec/requests/api/v1/restaurants_request_spec.rb
@@ -1,21 +1,36 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Restaurants API" do
-    xit "sends a list of restaurants" do
-        allow RestaurantsFacade.to_receive restaurants_near('xyz').and_return
-        [ restaurant_1 = create(:restaurant),
-        restaurant_2 = create(:restaurant),
-        restaurant_3 = create(:restaurant)]
-    
-        get '/api/v1/restaurants'
+RSpec.describe 'Restaurants API' do
+  it 'sends a list of restaurants', vcr: 'denver_restaurants' do
+    get '/api/v1/restaurants?search=Denver'
 
-        expect(response).to be_successful
-        items = JSON.parse(response.body, symbolize_names: true)
-        expect(items[:restaurant].count).to eq(3)
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    restaurants = JSON.parse(response.body, symbolize_names: true)
+    expect(restaurants.count).to eq(3)
+    expect(restaurants).to be_a(Array)
+    expect(restaurants).to be_all(Hash)
+
+    restaurants.each do |restaurant|
+      expect(restaurant).to have_key(:name)
+      expect(restaurant[:name]).to be_a(String)
+      expect(restaurant).to have_key(:rating)
+      expect(restaurant[:rating]).to be_a(Float)
+      expect(restaurant).to have_key(:price)
+      expect(restaurant[:price]).to be_a(String)
+      expect(restaurant).to have_key(:image_url)
+      expect(restaurant[:image_url]).to be_a(String)
+      expect(restaurant).to have_key(:url)
+      expect(restaurant[:url]).to be_a(String)
+      expect(restaurant).to have_key(:categories)
+      expect(restaurant[:categories]).to be_a(Array)
+      expect(restaurant).to have_key(:address)
+      expect(restaurant[:address]).to be_a(String)
+      expect(restaurant).to have_key(:phone)
+      expect(restaurant[:phone]).to be_a(String)
     end
-    it "returns Get /index", :vcr do
-        get '/api/v1/restaurants'
-        expect(response.body).to eq('{"status":"online"}')
-        expect(response.status).to eq(200)
-    end
+  end
 end
