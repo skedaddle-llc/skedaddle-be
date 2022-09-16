47dd699bad0d95dbd32ccf8d0ad6bf0ca0effa73
Eli Sachs <easachs13@gmail.com>
2022-09-16 13:57:40 -0600

    Test: create serializer specs

---
+++ spec/serializers/park_serializer_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ParkSerializer do
+  it 'can return a serialized instance of a poro', vcr: 'denver_parks' do
+    parks = ParksFacade.parks_near('Denver')
+    serializer = ParkSerializer.format_parks(parks)
+
+    expect(serializer).to be_a(Array)
+    serializer.each do |park|
+      expect(park[:name]).to be_a(String)
+      expect(park[:city]).to be_a(String)
+      expect(park[:state]).to be_a(String)
+      expect(park[:country]).to be_a(String)
+      expect(park[:description]).to be_a(String)
+      expect(park[:directions]).to be_a(String)
+      expect(park[:lat]).to be_a(String)
+      expect(park[:lon]).to be_a(String)
+      expect(park[:activities]).to be_a(Array)
+    end
+  end
+end
