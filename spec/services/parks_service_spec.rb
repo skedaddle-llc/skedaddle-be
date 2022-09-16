a99fca2e7318cbd4ce44de80ec343b692fdfe196
Nick Jones <nickjones9605@gmail.com>
2022-09-15 19:30:22 -0600

    Adds testing and sad path testing for Parksservice

--- spec/services/parks_service_spec.rb
+++ spec/services/parks_service_spec.rb
@@ -3,20 +3,20 @@ require 'rails_helper'
 RSpec.describe 'ParksService', :vcr do
 
     it "returns a 200 success header when the API call is made" do
-        response = ParksService.restaurants_near("Denver")
+        response = ParksService.parks_near("Denver")
         expect(response.first[1]).to_not(eq(nil))
         # expect(response["status"]).to(eq(200))   
     end
-    # it 'can return restaurants when the call is made' do
-    #     parse_json = RestaurantsService.restaurants_near("Denver")
+    xit 'can return restaurants when the call is made' do
+        parse_json = ParksService.parks_near("Denver")
 
-    #     expect(parse_json).to be_a(Hash)
-    #     expect(parse_json.first[1][0]).to include(:name, :rating, :phone, :categories)
-    # end
+        expect(parse_json).to be_a(Hash)
+        # expect(parse_json.first[1][0]).to include(:name, :city, :state, :country, :description, :directions, :lat, :lon, :activities)
+    end
 
-    # it 'can SAD PATH' do
-    #     parse_json = RestaurantsService.restaurants_near(nil)
+    it 'can SAD PATH' do
+        parse_json = ParksService.parks_near(nil)
         
-    #     expect(parse_json).to_not be_a(Array)
-    # end
+        expect(parse_json).to_not be_a(Array)
+    end
 end
