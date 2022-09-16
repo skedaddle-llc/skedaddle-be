14bc339a4a9727e9ead7b33dc1949456e50eb509
Wesley Miranda <wesleywes42@gmail.com>
2022-09-16 13:27:22 -0600

    makeing sure test work

--- app/serializers/park_serializer.rb
+++ app/serializers/park_serializer.rb
@@ -1,6 +1,19 @@
-# frozen_string_literal: true
-
 class ParkSerializer
-  include JSONAPI::Serializer
-  attributes :name, :city, :state, :country, :description, :directions, :lat, :lon
+  attr_reader :name, :city, :state, :country, :description, :directions, :lat, :lon, :activities
+  def self.format_parks(parks)
+    parks.map do |park|
+      {
+        name: park.name,
+        city: park.city,
+        state: park.state,
+        country: park.country,
+        description: park.description,
+        directions: park.directions,
+        lat: park.lat,
+        lon: park.lon,
+        activities: park.activities
+      }
+    end
+  end
+
 end
--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -2,6 +2,17 @@
 
 class RestaurantSerializer
   attr_reader :resource
-  include JSONAPI::Serializer
-  attributes :name, :rating, :price, :image_url, :url
+  # include JSONAPI::Serializer
+  # attributes :name, :rating, :price, :image_url, :url
+  def self.format_restaurant(restaurants)
+    restaurants.map do |restaurant|
+      {
+        name: restaurant.name,
+        rating: restaurant.rating,
+        price: restaurant.price,
+        image_url: restaurant.image_url,
+        url: restaurant.url,
+      }
+    end
+  end
 end
